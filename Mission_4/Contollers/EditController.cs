03383b992a4361bb530456cd07c9069fe54e34d1
= <zm275@byu.edu>
2023-02-06 22:04:57 -0700

    MVC move over is working

---
+++ Mission_4/Contollers/EditController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Contollers
+{
+    public class EditController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+    }
+}
