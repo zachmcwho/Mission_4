74488fbddf00d9290126172d2ca3023c1e9a3182
= <zm275@byu.edu>
2023-02-08 14:29:46 -0700

    validation*

--- Mission_4/Contollers/EditController.cs
+++ Mission_4/Contollers/EditController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mission_4.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,13 @@ namespace Mission_4.Contollers
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel model)
         {
             return View();
         }
---
+++ Mission_4/Models/CalculatorModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_4.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Assignment { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Quiz { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Group_Project { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Midterm { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Final { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "The value must be between 0 and 100.")]
+        public float Intex { get; set; }
+       
+    }
+}
